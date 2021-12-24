15563d07c5d9d8a0c012d9a5a721172b2d6c4247
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-12-24 10:34:28 +0700

    6.note_table_crud

--- src/main/kotlin/com/kylix/repository/Repository.kt
+++ src/main/kotlin/com/kylix/repository/Repository.kt
@@ -1,15 +1,16 @@
 package com.kylix.repository
 
+import com.kylix.model.Note
 import com.kylix.model.User
 import com.kylix.table.UserTable
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
+import com.kylix.repository.DatabaseFactory.dbQuery
+import com.kylix.table.NoteTable
+import org.jetbrains.exposed.sql.*
 
 class Repository {
 
     suspend fun addUser(user: User) {
-        DatabaseFactory.dbQuery {
+        dbQuery {
             UserTable.insert { table ->
                 table[UserTable.email] = user.email
                 table[UserTable.hashPassword] = user.hashPassword
@@ -18,7 +19,7 @@ class Repository {
         }
     }
 
-    suspend fun findUserByEmail(email: String) = DatabaseFactory.dbQuery {
+    suspend fun findUserByEmail(email: String) = dbQuery {
         UserTable.select {
             UserTable.email.eq(email)
         }
@@ -37,6 +38,58 @@ class Repository {
             email = row[UserTable.email],
             name = row[UserTable.name],
             hashPassword = row[UserTable.hashPassword]
+        )
+    }
+
+    suspend fun addNote(note: Note, email: String) {
+        dbQuery {
+            NoteTable.insert { noteTable ->
+                noteTable[NoteTable.id] = note.id
+                noteTable[NoteTable.userEmail] = email
+                noteTable[NoteTable.noteTitle] = note.noteTitle
+                noteTable[NoteTable.description] = note.description
+                noteTable[NoteTable.date] = note.date
+            }
+        }
+    }
+
+    suspend fun getAllNotes(email: String): List<Note> = dbQuery {
+        NoteTable.select {
+            NoteTable.userEmail.eq(email)
+        }.mapNotNull { rowToNote(it) }
+    }
+
+    suspend fun updateNote(note: Note, email: String) {
+        dbQuery {
+            NoteTable.update(
+                where = {
+                    NoteTable.userEmail.eq(email) and NoteTable.id.eq(note.id)
+                }
+            ) { noteTable ->
+                noteTable[NoteTable.noteTitle] = note.noteTitle
+                noteTable[NoteTable.description] = note.description
+                noteTable[NoteTable.date] = note.date
+            }
+        }
+    }
+
+    suspend fun deleteNote(id: String) {
+        dbQuery {
+            NoteTable.deleteWhere {
+                NoteTable.id.eq(id)
+            }
+        }
+    }
+
+    private fun rowToNote(row: ResultRow?): Note? {
+        if (row == null) {
+            return null
+        }
+        return Note(
+            id = row[NoteTable.id],
+            noteTitle =  row[NoteTable.noteTitle],
+            description = row[NoteTable.description],
+            date = row[NoteTable.date]
         )
     }
 }
