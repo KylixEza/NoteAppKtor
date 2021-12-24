cb307bd36d1cf8a634875ce04b2b7cd6d4a686b3
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-12-25 02:03:02 +0700

    fixing postgresql database connection

--- src/main/kotlin/com/kylix/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/kylix/repository/DatabaseFactory.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -31,10 +32,17 @@ object DatabaseFactory {
         val config = HikariConfig()
         config.apply {
             driverClassName = System.getenv("JDBC_DRIVER")
-            jdbcUrl = System.getenv("DATABASE_URL")
+            //jdbcUrl = System.getenv("DATABASE_URL")
             maximumPoolSize = 6
             isAutoCommit = false
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+            val uri = URI(System.getenv("DATABASE_URL"))
+            val username = uri.userInfo.split(":").toTypedArray()[0]
+            val password = uri.userInfo.split(":").toTypedArray()[1]
+
+            jdbcUrl = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "user=$username&password=$password"
+
             validate()
         }
         return HikariDataSource(config)
